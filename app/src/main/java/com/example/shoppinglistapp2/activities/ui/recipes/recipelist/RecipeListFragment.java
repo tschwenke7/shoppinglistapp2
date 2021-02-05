1c8fb15622763f0f24a604ec23b8a9bdf7cf9e4f
tschwenke <tschwenke7@gmail.com>
2021-02-05 13:13:37 +1100

    Replaces floating action button with actionbar + icon

--- app/src/main/java/com/example/shoppinglistapp2/activities/ui/recipes/recipelist/RecipeListFragment.java
+++ app/src/main/java/com/example/shoppinglistapp2/activities/ui/recipes/recipelist/RecipeListFragment.java
@@ -3,6 +3,9 @@ package com.example.shoppinglistapp2.activities.ui.recipes.recipelist;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -18,7 +21,7 @@ import com.example.shoppinglistapp2.activities.MainActivity;
 import com.example.shoppinglistapp2.activities.ui.recipes.RecipesViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class RecipesFragment extends Fragment implements RecipeListAdapter.OnRecipeClickListener{
+public class RecipeListFragment extends Fragment implements RecipeListAdapter.OnRecipeClickListener{
 
     private View root;
     private RecipesViewModel recipesViewModel;
@@ -30,6 +33,10 @@ public class RecipesFragment extends Fragment implements RecipeListAdapter.OnRec
 
         root = inflater.inflate(R.layout.fragment_recipe_list, container, false);
 
+        //setup action bar
+        this.setHasOptionsMenu(true);
+
+
         //setup recipe list recyclerview
         RecyclerView recipeRecyclerView = root.findViewById(R.id.recipe_recyclerview);
         final RecipeListAdapter adapter = new RecipeListAdapter(this);
@@ -39,12 +46,6 @@ public class RecipesFragment extends Fragment implements RecipeListAdapter.OnRec
         //set observer to update recipe list if it changes
         recipesViewModel.getAllRecipes().observe(getViewLifecycleOwner(), recipes -> {
             adapter.setRecipes(recipes);
-        });
-
-        //setup the new recipe button
-        FloatingActionButton newRecipeButton = root.findViewById(R.id.new_recipe_button);
-        newRecipeButton.setOnClickListener(view -> {
-            Navigation.findNavController(view).navigate(R.id.action_add_new_recipe);
         });
 
         return root;
@@ -58,6 +59,27 @@ public class RecipesFragment extends Fragment implements RecipeListAdapter.OnRec
         if (activity != null) {
             activity.hideUpButton();
         }
+    }
+
+    /** Merges extra menu items into the default activity action bar, according to provided menu xml */
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.recipe_list_action_bar, menu);
+    }
+
+    /** Handle onClick for the custom action bar menu items for this fragment */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_recipe:  {
+                // navigate to new recipe screen when "+" icon clicked
+                Navigation.findNavController(root).navigate(R.id.action_recipe_list_to_new_recipe);
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     @Override
