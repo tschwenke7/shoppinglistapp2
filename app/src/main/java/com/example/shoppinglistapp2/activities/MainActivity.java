3fad1d68ba2d0f0581880c18549d53e36f7f0a9d
tschwenke <tschwenke7@gmail.com>
2021-03-16 17:19:49 +1100

    Adds some transition animations

--- app/src/main/java/com/example/shoppinglistapp2/activities/MainActivity.java
+++ app/src/main/java/com/example/shoppinglistapp2/activities/MainActivity.java
@@ -9,6 +9,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -24,11 +25,31 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.recipe_list, R.id.shopping_list)
+                R.id.nav_recipe_list, R.id.nav_shopping_list)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+//        //add animations to bottom navigation bar
+//        NavOptions options = new NavOptions.Builder()
+//                .setLaunchSingleTop(true)
+//
+//                .setEnterAnim(R.anim.slide_in_right)
+//                .setExitAnim(R.anim.wait_anim)
+//                .build();
+//
+//        navView.setOnNavigationItemSelectedListener((item -> {
+//            switch(item.getItemId()){
+//                case R.id.nav_shopping_list:
+//                    navController.navigate(R.id.nav_shopping_list, null, options);
+//                    break;
+//                case R.id.nav_recipe_list:
+//                    navController.navigate(R.id.nav_recipe_list, null, options);
+//            }
+//            return true;
+//        }));
 
 
     }
