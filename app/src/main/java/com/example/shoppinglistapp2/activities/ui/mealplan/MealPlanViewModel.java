9e34913b081d4d4311426a087890fc15725bbaf5
tschwenke <tschwenke7@gmail.com>
2021-05-06 11:28:20 +1000

    Creates UI layouts for meal planner

---
+++ app/src/main/java/com/example/shoppinglistapp2/activities/ui/mealplan/MealPlanViewModel.java
@@ -0,0 +1,7 @@
+package com.example.shoppinglistapp2.activities.ui.mealplan;
+
+import androidx.lifecycle.ViewModel;
+
+public class MealPlanViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
