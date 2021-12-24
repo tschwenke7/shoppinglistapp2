de412d90cbe8d843cc61e114a42a56f84b86463d
tschwenke <tschwenke7@gmail.com>
2021-12-24 11:17:01 +1100

    Fix issue preventing clean db build and configured signed release build.

--- app/src/main/java/com/example/shoppinglistapp2/db/SlaDatabase.java
+++ app/src/main/java/com/example/shoppinglistapp2/db/SlaDatabase.java
@@ -30,11 +30,11 @@ import java.util.concurrent.Executors;
 
 @Database(entities = {Recipe.class, IngList.class, IngListItem.class, Tag.class, MealPlan.class, Meal.class},
         version = 17,
-        exportSchema = true,
-        autoMigrations = {
-                @AutoMigration(from = 15,to = 16),
-                @AutoMigration(from = 16, to = 17)
-        }
+        exportSchema = true
+//        autoMigrations = {
+//                @AutoMigration(from = 15,to = 16),
+//                @AutoMigration(from = 16, to = 17)
+//        }
 )
 public abstract class SlaDatabase extends RoomDatabase {
     public abstract RecipeDao recipeDao();
@@ -55,9 +55,8 @@ public abstract class SlaDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             SlaDatabase.class, "sla_database")
+                            .fallbackToDestructiveMigration()
                             .addCallback(sRoomDatabaseCallback)
-                            .addMigrations(MIGRATION_10_11, MIGRATION_11_12)
-//                            .fallbackToDestructiveMigration()
                             .build();
                 }
             }
@@ -72,10 +71,8 @@ public abstract class SlaDatabase extends RoomDatabase {
             super.onCreate(db);
             db.beginTransaction();
             try {
-                db.execSQL("DROP TABLE IF EXISTS slitems");
-                db.execSQL("DROP TABLE IF EXISTS ingredients");
-                db.execSQL("DROP TABLE IF EXISTS MealPlan");
                 db.execSQL("INSERT OR IGNORE INTO ing_lists DEFAULT VALUES");
+                db.setTransactionSuccessful();
             } catch(Exception e){
                 Log.d("TOM_TEST", e.toString());;
             }
@@ -85,126 +82,126 @@ public abstract class SlaDatabase extends RoomDatabase {
         }
     };
 
-    private static final Migration MIGRATION_7_8 = new Migration(7,8){
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL(new StringBuilder()
-                    .append("PRAGMA foreign_keys=off;")
-                    .append("BEGIN TRANSACTION;\n")
-                    .append("ALTER TABLE slitems RENAME TO old_table;\n")
-                    .append("CREATE TABLE slitems\n")
-                    .append("(\n")
-                    .append("id INTEGER PRIMARY KEY AUTOINCREMENT,\n")
-                    .append("name VARCHAR NOT NULL,\n")
-                    .append("qty1 VARCHAR,\n")
-                    .append("unit1 VARCHAR,\n")
-                    .append("qty2 VARCHAR,\n")
-                    .append("unit2 VARCHAR,\n")
-                    .append("checked INTEGER DEFAULT = '0'\n")
-                    .append(");\n")
-                    .append("INSERT INTO slitems SELECT * FROM old_table;\n")
-                    .append("COMMIT;\n")
-                    .append("PRAGMA foreign_keys=on;")
-                    .toString());
-        }
-    };
-
-    private static final Migration MIGRATION_9_10 = new Migration(9,10){
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL(new StringBuilder()
-                    .append("ALTER TABLE slitems ADD list_id INTEGER DEFAULT 2; \n")
-                    .append("UPDATE slitems SET list_id = 2; \n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("CREATE TABLE meal_plans\n")
-                    .append("(\n")
-                    .append("id INTEGER PRIMARY KEY AUTOINCREMENT,\n")
-                    .append("plan_id INTEGER DEFAULT 1,\n")
-                    .append("day_id INTEGER,\n")
-                    .append("day_title VARCHAR,\n")
-                    .append("recipe_id INTEGER REFERENCES recipe(id) ON DELETE CASCADE,\n")
-                    .append("notes VARCHAR\n")
-                    .append(");\n")
-                    .toString());
-        }
-    };
-    private static final Migration MIGRATION_10_11 = new Migration(10,11){
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL(new StringBuilder()
-                    .append("BEGIN TRANSACTION;\n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("ALTER TABLE meal_plans RENAME TO temp_table; \n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("CREATE TABLE meal_plans\n")
-                    .append("(\n")
-                    .append("id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n")
-                    .append("plan_id INTEGER DEFAULT 1 NOT NULL,\n")
-                    .append("day_id INTEGER NOT NULL,\n")
-                    .append("day_title VARCHAR,\n")
-                    .append("recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE,\n")
-                    .append("notes VARCHAR\n")
-                    .append(");\n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("INSERT INTO meal_plans(id, plan_id, day_id, day_title, recipe_id, notes)\n")
-                    .append("SELECT * FROM temp_table;\n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("COMMIT;")
-                    .toString());
-        }
-    };
-
-    private static final Migration MIGRATION_11_12 = new Migration(11,12){
-        @Override
-        public void migrate(@NonNull SupportSQLiteDatabase database) {
-            database.execSQL(new StringBuilder()
-                    .append("BEGIN TRANSACTION;\n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("DROP TABLE IF EXISTS temp_table;\n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("ALTER TABLE slitems RENAME TO temp_table; \n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("CREATE TABLE slitems\n")
-                    .append("(\n")
-                    .append("id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n")
-                    .append("list_id INTEGER DEFAULT 0 NOT NULL,\n")
-                    .append("name VARCHAR NOT NULL,\n")
-                    .append("qty1 VARCHAR,\n")
-                    .append("unit1 VARCHAR,\n")
-                    .append("qty2 VARCHAR,\n")
-                    .append("unit2 VARCHAR,\n")
-                    .append("checked INTEGER DEFAULT 0 NOT NULL\n")
-                    .append(");\n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("INSERT INTO slitems(id, list_id, name, qty1, unit1, qty2, unit2, checked)\n")
-                    .append("SELECT * FROM temp_table;\n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("DROP TABLE IF EXISTS temp_table;\n")
-                    .toString());
-
-            database.execSQL(new StringBuilder()
-                    .append("COMMIT;")
-                    .toString());
-        }
-    };
+//    private static final Migration MIGRATION_7_8 = new Migration(7,8){
+//        @Override
+//        public void migrate(@NonNull SupportSQLiteDatabase database) {
+//            database.execSQL(new StringBuilder()
+//                    .append("PRAGMA foreign_keys=off;")
+//                    .append("BEGIN TRANSACTION;\n")
+//                    .append("ALTER TABLE slitems RENAME TO old_table;\n")
+//                    .append("CREATE TABLE slitems\n")
+//                    .append("(\n")
+//                    .append("id INTEGER PRIMARY KEY AUTOINCREMENT,\n")
+//                    .append("name VARCHAR NOT NULL,\n")
+//                    .append("qty1 VARCHAR,\n")
+//                    .append("unit1 VARCHAR,\n")
+//                    .append("qty2 VARCHAR,\n")
+//                    .append("unit2 VARCHAR,\n")
+//                    .append("checked INTEGER DEFAULT = '0'\n")
+//                    .append(");\n")
+//                    .append("INSERT INTO slitems SELECT * FROM old_table;\n")
+//                    .append("COMMIT;\n")
+//                    .append("PRAGMA foreign_keys=on;")
+//                    .toString());
+//        }
+//    };
+//
+//    private static final Migration MIGRATION_9_10 = new Migration(9,10){
+//        @Override
+//        public void migrate(@NonNull SupportSQLiteDatabase database) {
+//            database.execSQL(new StringBuilder()
+//                    .append("ALTER TABLE slitems ADD list_id INTEGER DEFAULT 2; \n")
+//                    .append("UPDATE slitems SET list_id = 2; \n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("CREATE TABLE meal_plans\n")
+//                    .append("(\n")
+//                    .append("id INTEGER PRIMARY KEY AUTOINCREMENT,\n")
+//                    .append("plan_id INTEGER DEFAULT 1,\n")
+//                    .append("day_id INTEGER,\n")
+//                    .append("day_title VARCHAR,\n")
+//                    .append("recipe_id INTEGER REFERENCES recipe(id) ON DELETE CASCADE,\n")
+//                    .append("notes VARCHAR\n")
+//                    .append(");\n")
+//                    .toString());
+//        }
+//    };
+//    private static final Migration MIGRATION_10_11 = new Migration(10,11){
+//        @Override
+//        public void migrate(@NonNull SupportSQLiteDatabase database) {
+//            database.execSQL(new StringBuilder()
+//                    .append("BEGIN TRANSACTION;\n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("ALTER TABLE meal_plans RENAME TO temp_table; \n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("CREATE TABLE meal_plans\n")
+//                    .append("(\n")
+//                    .append("id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n")
+//                    .append("plan_id INTEGER DEFAULT 1 NOT NULL,\n")
+//                    .append("day_id INTEGER NOT NULL,\n")
+//                    .append("day_title VARCHAR,\n")
+//                    .append("recipe_id INTEGER REFERENCES recipes(id) ON DELETE CASCADE,\n")
+//                    .append("notes VARCHAR\n")
+//                    .append(");\n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("INSERT INTO meal_plans(id, plan_id, day_id, day_title, recipe_id, notes)\n")
+//                    .append("SELECT * FROM temp_table;\n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("COMMIT;")
+//                    .toString());
+//        }
+//    };
+//
+//    private static final Migration MIGRATION_11_12 = new Migration(11,12){
+//        @Override
+//        public void migrate(@NonNull SupportSQLiteDatabase database) {
+//            database.execSQL(new StringBuilder()
+//                    .append("BEGIN TRANSACTION;\n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("DROP TABLE IF EXISTS temp_table;\n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("ALTER TABLE slitems RENAME TO temp_table; \n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("CREATE TABLE slitems\n")
+//                    .append("(\n")
+//                    .append("id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n")
+//                    .append("list_id INTEGER DEFAULT 0 NOT NULL,\n")
+//                    .append("name VARCHAR NOT NULL,\n")
+//                    .append("qty1 VARCHAR,\n")
+//                    .append("unit1 VARCHAR,\n")
+//                    .append("qty2 VARCHAR,\n")
+//                    .append("unit2 VARCHAR,\n")
+//                    .append("checked INTEGER DEFAULT 0 NOT NULL\n")
+//                    .append(");\n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("INSERT INTO slitems(id, list_id, name, qty1, unit1, qty2, unit2, checked)\n")
+//                    .append("SELECT * FROM temp_table;\n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("DROP TABLE IF EXISTS temp_table;\n")
+//                    .toString());
+//
+//            database.execSQL(new StringBuilder()
+//                    .append("COMMIT;")
+//                    .toString());
+//        }
+//    };
 }
